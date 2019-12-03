20d0c91f1dbb565eb7bb74d88fbd2f9be2b0a4b8
GiulianiViviana <giuliani.viviana@iisgalvanimi.edu.it>
2019-12-03 11:58:13 +0000

    Inserimento riga per immagini

--- FAKERJS/routes/users.js
+++ FAKERJS/routes/users.js
@@ -13,11 +13,13 @@ function createFakePerson()
  let randomName = faker.name.findName(); // Rowan Nikolaus
  let randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
  let randomCard = faker.helpers.createCard(); // random contact card containing many properties
+ let randomImage = faker.image.people();
 
  let person = {
    name:randomName,
    email:randomEmail,
-   card: randomCard
+   card: randomCard,
+   image: randomImage
  }
  return person;
 }
